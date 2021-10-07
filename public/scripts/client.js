f976d283ad3f398eeeab3f8ad779435fdd27ac0a
Joseph Shin <seoung.shin@ontariotechu.net>
2021-10-07 16:33:10 -0400

    added ajax requests, removed data object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 // Fake data taken from initial-tweets.json
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  //AJAX refactor. form id is submit-tweet
+  $("#submit-tweet").submit(function(event)   {
+    event.preventDefault(); //prevents refresh/get.post
 
+    //what does the current query string look like?
+
+    //now need to serialize. need help
+    console.log($("#submit-tweet").serialize());
+
+    //AJAX post need help
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: $("#submit-tweet").serialize(),
+    })
+      .then(function(response) {
+        loadTweets();
+        console.log(response);
+      });
+
+  });
+
+  //function to prepend tweets to tweets container
   const renderTweets = function(tweets) {
     //might need to clear?
     for (let tweet of tweets) {
@@ -69,20 +68,14 @@ $(document).ready(function() {
 
     return $tweet;
   };
-  renderTweets(data);
+
+  //AJAX request to load twists
+  const loadTweets = function () {
+    $.ajax("/tweets").then(function(results) {
+      renderTweets(results);
+    });
+  };
+  loadTweets();
 });
 
-//AJAX refactor. form id is submit-tweet
-$("#submit-tweet").submit(function(event)   {
-  event.preventDefault(); //prevents refresh/get.post
 
-  //what does the current query string look like?
-
-  //now need to serialize. need help
-  console.log($(this).serialize());
-
-  //AJAX post need help
-  $.post( "ajax/test.html", function( data ) {
-    $( ".result" ).html( data );
-  });
-})
